e48de252a0960f9ee4d53ab86057161d08909e5f
ClemV <22980442+clementviolet@users.noreply.github.com>
2019-10-29 16:28:18 +0100

    Fix typo in  _build.sh

--- _build.sh
+++ _build.sh
@@ -2,4 +2,4 @@
 
 set -ev
 
-Rscript -e "rmarkdown::render('index.Rmd', output_format = rmdformats::readthedown()), output_file = `index.html`"
+Rscript -e "rmarkdown::render('index.Rmd', output_format = rmdformats::readthedown()), output_file = `index.html`)"
